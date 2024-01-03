75d3349333da0322dc67d59238f108750d1ce46b
zilene123 <145580953+zilene123@users.noreply.github.com>
2024-01-03 11:04:37 -0300

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,6 @@
+function capitalizarTitulos(livros){
+    let vetor=[];
+    livros.forEach(elelento=>{vetor.push(elelento)});
+    return vetor;
+}
+console.log(capitalizarTitulos('o pequeno principe','dom casmurro','a metamoforse'))
---
+++ q3.js
@@ -0,0 +1,10 @@
+let numero=Number('digite um numro:')
+if (numero<5){
+    console.log('numero baixo!');
+}
+else if(numero>5){
+    console.log('numero baixo!')
+}
+else{
+    console.log('acertou o numero, parabens')
+}
